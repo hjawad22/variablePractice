7c761c22c1d28d12f83dc059a2538b1d8ba2eb89
Houda Jawad <houdajawad@Houdas-MBP.attlocal.net>
2022-12-05 10:45:03 -0800

    Add revision

--- variables.js
+++ variables.js
@@ -1,14 +1,11 @@
 var email = "hithere@gmail.com"
 var userName = "bob"
-
 var city = "sacramento"
 
-console.log(email) 
-var email = "wish@gmail.com"
-console.log(userName)
-var userName = "linda"
-console.log(city)
-var city = "san diego"
+console.log(email) = "wish@gmail.com"
+console.log(userName) = "sue"
+console.log(city) = "san diego"
+
 
 var currentAge = 20
 var studentId = 109 
